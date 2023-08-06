25ded8b85667793d359dbf479bf18002a59a3323
Kévine NASSARA <vinenassara@gmail.com>
2023-08-06 13:57:21 +0100

    Add projects and services in my portfolio

--- resources/views/pages/partials/Project.blade.php
+++ resources/views/pages/partials/Project.blade.php
@@ -4,43 +4,72 @@
             <div class="col-lg-6">
                 <h1 class="display-5 mb-0">My Projects</h1>
             </div>
+            /* The code you provided is creating a section with a list of filters for the projects. */
             <div class="col-lg-6 text-lg-end">
                 <ul class="list-inline mx-n3 mb-0" id="portfolio-flters">
                     <li class="mx-3 active" data-filter="*">All Projects</li>
                     <li class="mx-3" data-filter=".first">UI/UX Design</li>
-                    <li class="mx-3" data-filter=".second">Graphic Design</li>
+                    <li class="mx-3" data-filter=".second">Web Project</li>
+                    <li class="mx-3" data-filter=".three">Mobile Project</li>
                 </ul>
             </div>
         </div>
         <div class="row g-4 portfolio-container wow fadeInUp" data-wow-delay="0.1s">
             <div class="col-lg-4 col-md-6 portfolio-item first">
                 <div class="portfolio-img rounded overflow-hidden">
-                    <img class="img-fluid" src="{{ asset('pages/asset/img/project-1.jpg')}}" alt="">
+                    <img class="img-fluid" src="{{ asset('pages/asset/Image_graphisme/Fashion.png')}}" alt="">
                     <div class="portfolio-btn">
-                        <a class="btn btn-lg-square btn-outline-secondary border-2 mx-1" href="{{ asset('pages/asset/img/project-1.jpg')}}" data-lightbox="portfolio"><i class="fa fa-eye"></i></a>
+                        <a class="btn btn-lg-square btn-outline-secondary border-2 mx-1" href="{{ asset('pages/asset/Image_graphisme/Fashion.png')}}" data-lightbox="portfolio"><i class="fa fa-eye"></i></a>
                         <a class="btn btn-lg-square btn-outline-secondary border-2 mx-1" href=""><i class="fa fa-link"></i></a>
                     </div>
                 </div>
             </div>
             <div class="col-lg-4 col-md-6 portfolio-item second">
                 <div class="portfolio-img rounded overflow-hidden">
-                    <img class="img-fluid" src="{{ asset('pages/asset/img/project-2.jpg')}}" alt="">
+                    <img class="img-fluid" src="{{ asset('pages/asset/Image_site_web/training.png')}}" alt="">
                     <div class="portfolio-btn">
-                        <a class="btn btn-lg-square btn-outline-secondary border-2 mx-1" href="{{ asset('pages/asset/img/project-2.jpg')}}" data-lightbox="portfolio"><i class="fa fa-eye"></i></a>
+                        <a class="btn btn-lg-square btn-outline-secondary border-2 mx-1" href="{{ asset('pages/asset/Image_site_web/training.png')}}" data-lightbox="portfolio"><i class="fa fa-eye"></i></a>
+                        <a class="btn btn-lg-square btn-outline-secondary border-2 mx-1" href="https://jcisoleil.banelsems.com/"><i class="fa fa-link"></i></a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6 portfolio-item first">
+                <div class="portfolio-img rounded overflow-hidden">
+                    <img class="img-fluid" src="{{ asset('pages/asset/Image_graphisme/Dreams Cleaning.png')}}" alt="">
+                    <div class="portfolio-btn">
+                        <a class="btn btn-lg-square btn-outline-secondary border-2 mx-1" href="{{ asset('pages/asset/Image_graphisme/Dreams_Cleaning.png')}}" data-lightbox="portfolio"><i class="fa fa-eye"></i></a>
+                        <a class="btn btn-lg-square btn-outline-secondary border-2 mx-1" href=""><i class="fa fa-link"></i></a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6 portfolio-item second">
+                <div class="portfolio-img rounded overflow-hidden">
+                    <img class="img-fluid" src="{{ asset('pages/asset/Image_site_web/OnlineExchange.png')}}" alt="">
+                    <div class="portfolio-btn">
+                        <a class="btn btn-lg-square btn-outline-secondary border-2 mx-1" href="{{ asset('pages/asset/Image_site_web/OnlineExchange.png')}}" data-lightbox="portfolio"><i class="fa fa-eye"></i></a>
                         <a class="btn btn-lg-square btn-outline-secondary border-2 mx-1" href=""><i class="fa fa-link"></i></a>
                     </div>
                 </div>
             </div>
             <div class="col-lg-4 col-md-6 portfolio-item first">
                 <div class="portfolio-img rounded overflow-hidden">
-                    <img class="img-fluid" src="{{ asset('pages/asset/img/project-3.jpg')}}" alt="">
+                    <img class="img-fluid" src="{{ asset('pages/asset/Image_graphisme/orientation.jpg')}}" alt="">
                     <div class="portfolio-btn">
-                        <a class="btn btn-lg-square btn-outline-secondary border-2 mx-1" href="{{ asset('pages/asset/img/project-3.jpg')}}" data-lightbox="portfolio"><i class="fa fa-eye"></i></a>
+                        <a class="btn btn-lg-square btn-outline-secondary border-2 mx-1" href="{{ asset('pages/asset/Image_graphisme/orientation.jpg')}}" data-lightbox="portfolio"><i class="fa fa-eye"></i></a>
                         <a class="btn btn-lg-square btn-outline-secondary border-2 mx-1" href=""><i class="fa fa-link"></i></a>
                     </div>
                 </div>
             </div>
             <div class="col-lg-4 col-md-6 portfolio-item second">
+                <div class="portfolio-img rounded overflow-hidden">
+                    <img class="img-fluid" src="{{ asset('pages/asset/Image_site_web/any_course.png')}}" alt="">
+                    <div class="portfolio-btn">
+                        <a class="btn btn-lg-square btn-outline-secondary border-2 mx-1" href="{{ asset('pages/asset/Image_site_web/any_course.png')}}" data-lightbox="portfolio"><i class="fa fa-eye"></i></a>
+                        <a class="btn btn-lg-square btn-outline-secondary border-2 mx-1" href=""><i class="fa fa-link"></i></a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6 portfolio-item three">
                 <div class="portfolio-img rounded overflow-hidden">
                     <img class="img-fluid" src="{{ asset('pages/asset/img/project-4.jpg')}}" alt="">
                     <div class="portfolio-btn">
@@ -49,7 +78,7 @@
                     </div>
                 </div>
             </div>
-            <div class="col-lg-4 col-md-6 portfolio-item first">
+            <div class="col-lg-4 col-md-6 portfolio-item three">
                 <div class="portfolio-img rounded overflow-hidden">
                     <img class="img-fluid" src="{{ asset('pages/asset/img/project-5.jpg')}}" alt="">
                     <div class="portfolio-btn">
@@ -58,11 +87,29 @@
                     </div>
                 </div>
             </div>
-            <div class="col-lg-4 col-md-6 portfolio-item second">
+            <div class="col-lg-4 col-md-6 portfolio-item three">
                 <div class="portfolio-img rounded overflow-hidden">
                     <img class="img-fluid" src="{{ asset('pages/asset/img/project-6.jpg')}}" alt="">
                     <div class="portfolio-btn">
                         <a class="btn btn-lg-square btn-outline-secondary border-2 mx-1" href="{{ asset('pages/asset/img/project-6.jpg')}}" data-lightbox="portfolio"><i class="fa fa-eye"></i></a>
+                        <a class="btn btn-lg-square btn-outline-secondary border-2 mx-1" href=""><i class="fa fa-link"></i></a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6 portfolio-item second">
+                <div class="portfolio-img rounded overflow-hidden">
+                    <img class="img-fluid" src="{{ asset('pages/asset/Image_site_web/music_app.png')}}" alt="">
+                    <div class="portfolio-btn">
+                        <a class="btn btn-lg-square btn-outline-secondary border-2 mx-1" href="{{ asset('pages/asset/Image_site_web/music_app.png')}}" data-lightbox="portfolio"><i class="fa fa-eye"></i></a>
+                        <a class="btn btn-lg-square btn-outline-secondary border-2 mx-1" href=""><i class="fa fa-link"></i></a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6 portfolio-item first">
+                <div class="portfolio-img rounded overflow-hidden">
+                    <img class="img-fluid" src="{{ asset('pages/asset/Image_graphisme/Electricien.png')}}" alt="">
+                    <div class="portfolio-btn">
+                        <a class="btn btn-lg-square btn-outline-secondary border-2 mx-1" href="{{ asset('pages/asset/Image_graphisme/Electricien.png')}}" data-lightbox="portfolio"><i class="fa fa-eye"></i></a>
                         <a class="btn btn-lg-square btn-outline-secondary border-2 mx-1" href=""><i class="fa fa-link"></i></a>
                     </div>
                 </div>
--- resources/views/pages/partials/Service.blade.php
+++ resources/views/pages/partials/Service.blade.php
@@ -9,51 +9,91 @@
             </div>
         </div>
         <div class="row g-4">
-            <div class="col-lg-6 wow fadeInUp" data-wow-delay="0.1s">
-                <div class="service-item d-flex flex-column flex-sm-row bg-white rounded h-100 p-4 p-lg-5">
-                    <div class="bg-icon flex-shrink-0 mb-3">
-                        <i class="fa fa-crop-alt fa-2x text-dark"></i>
-                    </div>
-                    <div class="ms-sm-4">
-                        <h4 class="mb-3">Creative Design</h4>
-                        <h6 class="mb-3">Start from <span class="text-primary">$199</span></h6>
-                        <span>Stet lorem dolor diam amet vero eos. No stet est diam amet diam ipsum. Clita dolor duo clita sit sed sit dolor eos.</span>
-                    </div>
-                </div>
-            </div>
             <div class="col-lg-6 wow fadeInUp" data-wow-delay="0.3s">
                 <div class="service-item d-flex flex-column flex-sm-row bg-white rounded h-100 p-4 p-lg-5">
                     <div class="bg-icon flex-shrink-0 mb-3">
                         <i class="fa fa-code-branch fa-2x text-dark"></i>
                     </div>
                     <div class="ms-sm-4">
-                        <h4 class="mb-3">Graphic Design</h4>
-                        <h6 class="mb-3">Start from <span class="text-primary">$199</span></h6>
-                        <span>Stet lorem dolor diam amet vero eos. No stet est diam amet diam ipsum. Clita dolor duo clita sit sed sit dolor eos.</span>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-6 wow fadeInUp" data-wow-delay="0.1s">
-                <div class="service-item d-flex flex-column flex-sm-row bg-white rounded h-100 p-4 p-lg-5">
-                    <div class="bg-icon flex-shrink-0 mb-3">
-                        <i class="fa fa-code fa-2x text-dark"></i>
-                    </div>
-                    <div class="ms-sm-4">
-                        <h4 class="mb-3">Web Design</h4>
-                        <h6 class="mb-3">Start from <span class="text-primary">$199</span></h6>
-                        <span>Stet lorem dolor diam amet vero eos. No stet est diam amet diam ipsum. Clita dolor duo clita sit sed sit dolor eos.</span>
+                        <h4 class="mb-3">Conception graphique</h4>
+                        <h6 class="mb-3">Créez une visibilité à travers les affiches, flyers et logos percutants. {{-- <span class="text-primary">$20</span> --}}</h6>
+                        <span>
+                            Plongez dans un monde de créativité en faisant appel à mes services en tant que graphiste professionnel. 
+                            Maîtrisant l'art de la composition, des couleurs et des typographies, je crée des illustrations envoûtantes, des infographies percutantes et des designs web attrayants qui subliment votre communication visuelle et vous démarquent de la concurrence 
+                        </span>
                     </div>
                 </div>
             </div>
             <div class="col-lg-6 wow fadeInUp" data-wow-delay="0.3s">
                 <div class="service-item d-flex flex-column flex-sm-row bg-white rounded h-100 p-4 p-lg-5">
                     <div class="bg-icon flex-shrink-0 mb-3">
+                        <i class="fa fa-code fa-2x text-dark"></i>
+                    </div>
+                    <div class="ms-sm-4">
+                        <h4 class="mb-3">Développeur mobile</h4>
+                        <h6 class="mb-3">Créez des applications mobiles  fluides et intuitives. {{-- <span class="text-primary">$199</span> --}}</h6>
+                        <span>
+                            Démarquez-vous sur le marché mobile avec mon aide en tant que développeur passionné, spécialisé dans le développement d'applications mobiles. Je mets à profit mon expertise en utilisant Laravel en backend et Flutter en front-end, en intégrant habilement des APIs avancées. Cela me permet de vous fournir des applications natives et hybrides performantes, esthétiques et fonctionnelles qui feront sensation auprès de votre public cible.
+                        </span>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-6 wow fadeInUp" data-wow-delay="0.1s">
+                <div class="service-item d-flex flex-column flex-sm-row bg-white rounded h-100 p-4 p-lg-5">
+                    <div class="bg-icon flex-shrink-0 mb-3">
+                        <i class="fa fa-crop-alt fa-2x text-dark"></i>
+                    </div>
+                    <div class="ms-sm-4">
+                        <h4 class="mb-3">Administrateur de base de donnée</h4>
+                        <h6 class="mb-3">Concevoir des bases de données sécurisées et performantes {{-- <span class="text-primary">$199</span> --}}</h6>
+                        <span>
+                            Confiez la gestion et l'optimisation de vos bases de données à un administrateur chevronné tel que moi. Avec une expertise approfondie dans les systèmes de gestion de bases de données (SGBD) tels que MySQL, SQLite, et Firebase, je veille à la fiabilité, à la sécurité et à l'efficacité de vos données, garantissant ainsi des performances optimales pour votre entreprise.
+                        </span>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-lg-6 wow fadeInUp" data-wow-delay="0.1s">
+                <div class="service-item d-flex flex-column flex-sm-row bg-white rounded h-100 p-4 p-lg-5">
+                    <div class="bg-icon flex-shrink-0 mb-3">
                         <i class="fa fa-laptop-code fa-2x text-dark"></i>
                     </div>
                     <div class="ms-sm-4">
-                        <h4 class="mb-3">UI/UX Design</h4>
-                        <h6 class="mb-3">Start from <span class="text-primary">$199</span></h6>
-                        <span>Stet lorem dolor diam amet vero eos. No stet est diam amet diam ipsum. Clita dolor duo clita sit sed sit dolor eos.</span>
+                        <h4 class="mb-3">Développeur Web</h4>
+                        <h6 class="mb-3">Créez une présence en ligne exceptionnelle {{-- <span class="text-primary">$199</span> --}}</h6>
+                        <span>
+                            Découvrez comment je peux créer pour vous des solutions web sur mesure et innovantes en utilisant les puissants frameworks Laravel et React. Fort de mon expérience en développement web, je suis capable de concevoir des sites performants, des applications interactives et des expériences utilisateur uniques, répondant ainsi à tous vos besoins numériques.
+                        </span>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-lg-6 wow fadeInUp" data-wow-delay="0.3s">
+                <div class="service-item d-flex flex-column flex-sm-row bg-white rounded h-100 p-4 p-lg-5">
+                    <div class="bg-icon flex-shrink-0 mb-3">
+                        <i class="fa fa-code fa-2x text-dark"></i>
+                    </div>
+                    <div class="ms-sm-4">
+                        <h4 class="mb-3">Concepteur et Testeur d'API </h4>
+                        <h6 class="mb-3">Créez des APIs plus test. {{-- <span class="text-primary">$199</span> --}}</h6>
+                        <span>
+                            Découvrez comment je peux donner vie à vos idées grâce à mes services de concepteur et testeur d'API. Fort de mon expertise en conception d'interfaces de programmation, je développe des API robustes et flexibles pour faciliter l'échange de données entre vos applications, tout en assurant leur qualité par des tests rigoureux.
+                        </span>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-lg-6 wow fadeInUp" data-wow-delay="0.1s">
+                <div class="service-item d-flex flex-column flex-sm-row bg-white rounded h-100 p-4 p-lg-5">
+                    <div class="bg-icon flex-shrink-0 mb-3">
+                        <i class="fa fa-crop-alt fa-2x text-dark"></i>
+                    </div>
+                    <div class="ms-sm-4">
+                        <h4 class="mb-3">DevOps</h4>
+                        <h6 class="mb-3">Déploiement web continu {{-- <span class="text-primary">$199</span> --}}</h6>
+                        <span>
+                            Débloquez le plein potentiel de votre infrastructure logicielle avec mes compétences DevOps. En tant qu'expert en automatisation, intégration continue et déploiement continu, je façonne des environnements agiles qui permettent une évolutivité sans faille, des mises à jour sans interruption, et une gestion proactive des incidents pour assurer la stabilité et la haute disponibilité de vos applications.
+                        </span>
                     </div>
                 </div>
             </div>
